a1962030f098bfa16d5037e0ccb8b89d82daebcb
HarikrishnaThoka <harikrishna.thoka@trustpayments.com>
2023-02-19 22:57:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+@app.get('/')
+async def root():
+    return {"Hello":"World"}
+
+import nest_asyncio
+from pyngrok import ngrok
+import uvicorn
+
+ngrok_tunnel = ngrok.connect(8000)
+print('Public URL:', ngrok_tunnel.public_url)
+nest_asyncio.apply()
+uvicorn.run(app, port=8000)
